1b1318b2fc4b9a4cad5ecbe6222932b8155f70c7
Timothe Tinguely <147623034+Timiboylebg@users.noreply.github.com>
2024-05-13 09:55:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,14 @@ import pandas as pd
 import plotly.express as px
 
 
-training_data = pd.read_csv("https://raw.githubusercontent.com/vgentile98/text_difficulty_prediction/main/data/training_data.csv")
-test_data = pd.read_csv("https://raw.githubusercontent.com/vgentile98/text_difficulty_prediction/main/data/unlabelled_test_data.csv")
+st.title('Recherche de Médias')
 
+# Créer une barre de recherche
+query = st.text_input("Entrez votre recherche:", "")
+
+# Vous pouvez afficher la requête entrée par l'utilisateur pour confirmation
+if query:
+    st.write(f"Vous avez recherché: {query}")
 
 url = 'https://raw.githubusercontent.com/michalis0/MGT-502-Data-Science-and-Machine-Learning/main/data/yield_df.csv'
 df_yield= pd.read_csv(url)
@@ -14,10 +19,6 @@ df_yield= pd.read_csv(url)
 data = df_yield['Item'].value_counts().reset_index()
 data.columns = ['Item', 'count']  # Rename columns for clarity
 
-# Streamlit app
-st.title('Shares of crops')
-
-st.title('timothe le bg')
 
 # Create a pie chart using Plotly Express
 fig = px.pie(data, names='Item', values='count', title='Shares of crops')
