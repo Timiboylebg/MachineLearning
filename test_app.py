e8909fb425dc3c7e531bbf84b8b1dc54ae98ac52
Timothe Tinguely <147623034+Timiboylebg@users.noreply.github.com>
2024-05-14 14:49:30 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,15 +8,10 @@ from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptFoun
 import requests
 
 
-api_key = 'AIzaSyBHktH6XIGar0v7UStL1reXQUyx8aqxHwg'
-news_api_key = '00bd707fafb54308842886874d3b23a5'
-api_service_name = 'youtube'
-api_version = 'v3'
 
 st.title('Search for a media that interest you')
 
 
-st.button("Show Transcript", key=video_id)
 
 # Key must be unique for each button
 
