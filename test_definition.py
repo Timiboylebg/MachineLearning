47473502d62f3d3f9174e52888382aa0231f97b9
Timothe Tinguely <147623034+Timiboylebg@users.noreply.github.com>
2024-05-17 14:35:26 +0200

    Update test_definition.py

--- test_definition.py
+++ test_definition.py
@@ -44,7 +44,7 @@ if word_en:
     if definition:
         translation_fr = translate_to_french(definition)
         st.write(f"**Definition of {word_en} in French:**")
-        st.write(translation_fr)
+        st.write(f"{word_en} = {translation_fr}")
     else:
         st.write("No definition found.")
 else:
@@ -59,7 +59,7 @@ if word_fr:
         definition_en = get_single_definition(translation_en)
         if definition_en:
             st.write(f"**Definition of {word_fr} (translated to {translation_en}) in English:**")
-            st.write(definition_en)
+            st.write(f"{translation_en} = {definition_en}")
         else:
             st.write("No definition found for the translated word.")
     else:
